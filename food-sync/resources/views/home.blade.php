e8b7fa0ce66ac9e1bba72812076d5b64e27085e9
Pot4toChips <tivolt.ricsi@gmail.com>
2025-11-11 20:35:20 +0100

    Refactored auth pages

--- food-sync/resources/views/home.blade.php
+++ food-sync/resources/views/home.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Food Sync - Home')
 
 @section('css')
-<link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/home.css">
 @endsection
 
 @section('content')
-<div id="recipe-posts" class="container-fluid p-0 d-flex flex-column justify-content-center align-items-center"></div>
+    <div id="recipe-posts" class="container-fluid p-0 d-flex flex-column justify-content-center align-items-center"></div>
 @endsection
 
 @section('js')
-<script src="js/home.js"></script>
+    <script src="js/home.js"></script>
 @endsection

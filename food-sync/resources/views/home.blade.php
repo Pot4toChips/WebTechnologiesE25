5f88f68875dd33acf7524621dbe56112d141cc49
Pot4toChips <tivolt.ricsi@gmail.com>
2025-11-13 13:20:21 +0100

    Added recipe post UI

--- food-sync/resources/views/home.blade.php
+++ food-sync/resources/views/home.blade.php
@@ -7,6 +7,42 @@
 @endsection
 
 @section('content')
+    <div id="recipe-post-editor" class="content-card sticky-top accordion accordion-flush w-100 mb-3 p-0">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Share your Recipes
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#recipe-post-editor">
+                <div class="accordion-body">
+                    <form method="POST" action="">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label for="recipe-title" class="form-label">Title</label>
+                            <input type="text" class="form-control" name="recipe-title" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="recipe-ingredients" class="form-label">Ingredients</label>
+                            <textarea type="text" class="form-control" name="recipe-ingredients" required></textarea>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="recipe-instructions" class="form-label">Instructions</label>
+                            <textarea type="text" class="form-control" name="recipe-instructions" required></textarea>
+                        </div>
+
+                        <x-input-error :messages="$errors->get('email')" />
+                        <x-input-error :messages="$errors->get('password')" />
+
+                        <button type="submit" class="btn btn-primary px-4">Share</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <div id="recipe-posts" class="container-fluid p-0 d-flex flex-column justify-content-center align-items-center"></div>
 @endsection
 
--- food-sync/routes/web.php
+++ food-sync/routes/web.php
@@ -12,6 +12,7 @@ Route::get('/home', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
 
+// Page Routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

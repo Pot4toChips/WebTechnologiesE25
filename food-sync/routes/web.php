07ae4ab9c37ee0fbadb1fdd802edbaefca3f168a
roman <romanteren06@gmail.com>
2025-11-14 12:45:33 +0100

    prevent overwriting

--- food-sync/routes/web.php
+++ food-sync/routes/web.php
@@ -12,8 +12,12 @@ Route::get('/home', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
 
+Route::get('/profile', function() {
+    return view('profile');
+});
+
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });

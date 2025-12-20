0190acec53ef1717412103ec4d07817e24063707
Pot4toChips <tivolt.ricsi@gmail.com>
2025-12-21 00:41:24 +0100

    Remove default laravel pages

--- food-sync/routes/web.php
+++ food-sync/routes/web.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-}); 
+    return redirect()->route('home');
+});
 
 Route::get('/home', function () {
     return view('home.home');
